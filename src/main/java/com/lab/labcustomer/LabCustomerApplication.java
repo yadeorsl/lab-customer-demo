f1cacb11962298887168c24e0bd639615428a089
yadeorsl <yadeorsl@gmail.com>
2024-03-05 16:32:58 +0300

    init project

---
+++ src/main/java/com/lab/labcustomer/LabCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.lab.labcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCustomerApplication.class, args);
+	}
+
+}
