26c84ac677bea8d794a31582f4560c5d98e1f76d
yadeorsl <yadeorsl@gmail.com>
2024-03-08 16:21:09 +0300

    Add getAll & getFindByID method

--- src/main/java/com/lab/labcustomer/controller/CustomerController.java
+++ src/main/java/com/lab/labcustomer/controller/CustomerController.java
@@ -3,11 +3,8 @@ package com.lab.labcustomer.controller;
 import com.lab.labcustomer.model.Customer;
 import com.lab.labcustomer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 @RestController
 @RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
@@ -19,4 +16,13 @@ private final CustomerService customerService;
 
     return customerService.saveCustomer(customer);
 }
+  @GetMapping
+    public List<Customer> allCustomer(){
+   return  customerService.allCustomer();
+  }
+  @GetMapping("{id}")
+    public Customer getCustomerByID(@PathVariable("id") long id){
+     return customerService.getCustomerFingById(id);
+  }
+
 }
--- src/main/java/com/lab/labcustomer/service/CustomerService.java
+++ src/main/java/com/lab/labcustomer/service/CustomerService.java
@@ -3,13 +3,32 @@ package com.lab.labcustomer.service;
 import com.lab.labcustomer.model.Customer;
 import com.lab.labcustomer.respository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerService {
  private final CustomerRepository customerRepository;
     public Customer saveCustomer(Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    public List<Customer> allCustomer() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerFingById(long id) {
+        Optional<Customer> customer=customerRepository.findById(id);
+       if (customer.isPresent()){
+           return customer.get();
+       }else {
+           log.info("Aradiginiz id de kullanici yok");
+           return null;
+       }
     }
 }
